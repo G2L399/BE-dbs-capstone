896549fd97861717ac4bed6f0aa73c8bc75a7647
Gamer2Language <bagassatwi@gmail.com>
2025-04-11 21:07:56 +0700

    feat: add login and signup endpoints with JWT authentication and user validation

---
+++ src/POST/login/app.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+import Hapi from "@hapi/hapi";
+import jwt from "jsonwebtoken";
+const prisma = new PrismaClient();
+export default async (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
+  const { email, password } = request.payload as any;
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (email && password) {
+    if (password == user?.password) {
+      const token = jwt.sign(
+        {
+          id: user?.id,
+        },
+        process.env.JWT_SECRET as string,
+        {
+          expiresIn: "1h",
+        }
+      );
+      return h
+        .response({ message: "Login successful", email, token })
+        .code(200);
+    } else {
+      return h.response({ message: "Invalid email or password" }).code(401);
+    }
+  } else {
+    return h.response({ message: "Email and password are required" }).code(400);
+  }
+};
