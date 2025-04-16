928a560d9ad7983fd13354b7372edd49fc869b02
DerylFeyza <muhammadderylfeyza@gmail.com>
2025-04-16 10:19:50 +0700

    remove unused user controller

--- src/controllers/user.ts
+++
@@ -1,7 +0,0 @@
-import Hapi from '@hapi/hapi';
-export const welcome = (
-  request: Hapi.Request<Hapi.ReqRefDefaults>,
-  h: Hapi.ResponseToolkit<Hapi.ReqRefDefaults>
-) => {
-  return 'PATCH request to /user';
-};
